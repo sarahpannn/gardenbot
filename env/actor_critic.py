102e2abe8438165ca3c840a5eee5ddde3ee55f29
sarahppp <sarahpan888@gmail.com>
2023-03-18 19:29:47 -0700

    first commit :p

---
+++ env/actor_critic.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+
+import torchvision.models as models
+
+class ActorCritic(nn.module):
+    def __init__(self, hidden_size):
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.actions = {
+            "water": -1,
+            "sow_seed": 1,
+            "measure_soil_moisture": 0,
+            "take_photo": 0,
+            "suppress_weed": 1
+        }
+        # table should be 2:1 ratio
+        # -> input 256 x 512
+        self.actor_bb = models.resnet34(pretrained=True)
+        
+    
+        
---
+++ env/reward_func.py
@@ -0,0 +1,6 @@
+def reward(state):
+    """The reward function takes in 
+
+    Args:
+        state (_type_): _description_
+    """
