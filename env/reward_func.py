7bbde45e3b20d4cb0617283dfd8c7150f7a6eb7f
sarahppp <sarahpan888@gmail.com>
2023-03-18 19:58:31 -0700

    reward stuff

--- env/reward_func.py
+++ env/reward_func.py
@@ -1,6 +1,19 @@
-def reward(state):
-    """The reward function takes in 
-
-    Args:
-        state (_type_): _description_
+class reward():
+    """The reward function class generates an instantaneous reward given a 
+    particular state based on the following parameters
+    
+    1. Legality of state:
+        - x for x overlaps
+    
+    2. Conservation of water:
+        - for each water used
+        
+    3. Diversity of garden:
+        + x for x > 3 plant species
+        0 for 3 species
+        - x for x < 3 plant species
+    
+    4. Density of garden
+        +% for %density > 75
+        -% for %density < 75
     """
